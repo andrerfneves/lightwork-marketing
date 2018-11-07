f708bdda9ea71d91d67f4ab220005fa85969bdee
Andre Neves <andre@bighuman.com>
2018-11-06 22:08:49 -0500

    feature: implement coming soon view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import logoImage from './logo.png';
+import './index.scss';
+
+const App = () => (
+  <div className='lightwork'>
+    <div className='lightwork__container'>
+      <div className='lightwork__logo'>
+        <img
+          src={logoImage}
+          className='lightwork__logo-image'
+          alt='Lightwork'
+        />
+        <div className='lightwork__logo-text'>
+          lightwork
+        </div>
+      </div>
+      <div className='lightwork__description'>
+        Task Board powered by the Lightning Network
+      </div>
+      <div className='lightwork__coming-soon'>
+        coming soon
+      </div>
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
